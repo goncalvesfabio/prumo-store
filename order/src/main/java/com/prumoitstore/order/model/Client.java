e5f20617ba4fe05cd54f96fba781a723b5943b00
Marcio Souza <marciooliveira10@gmail.com>
2022-05-09 09:38:08 +0100

    feat: created database.

---
+++ order/src/main/java/com/prumoitstore/order/model/Client.java
@@ -0,0 +1,22 @@
+package com.prumoitstore.order.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+}

a4c885fd98fffea2411c44166083bb4bb8ffb259
Marcio Souza <marciooliveira10@gmail.com>
2022-05-13 16:47:59 +0100

    feat: Improvements

--- orders/src/main/java/com/prumoitstore/order/OrderApplication.java
+++ orders/src/main/java/com/prumoitstore/order/OrderApplication.java
@@ -1,9 +1,7 @@
 package com.prumoitstore.order;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 //@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
--- orders/src/main/java/com/prumoitstore/order/domain/Address.java
+++ orders/src/main/java/com/prumoitstore/order/domain/Address.java
@@ -1,4 +1,4 @@
-package com.prumoitstore.order.model;
+package com.prumoitstore.order.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;

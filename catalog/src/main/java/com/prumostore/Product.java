6b54dd6238c27a0adf48322e4b339357df9d598d
Lalesca Dias <lalescadias6@gmail.com>
2022-05-16 22:24:13 +0100

    Create the classes to represent the database Order entities using JPA

--- catalog/src/main/java/com/prumostore/Product.java
+++ catalog/src/main/java/com/prumostore/Product.java
@@ -1,11 +1,9 @@
 package com.prumostore;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity //
-public class product {
+@Entity//
+@Table(name = "products")
+public class Product {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long productId;
--- catalog/src/main/java/com/prumostore/catalog/CatalogApplication.java
+++ catalog/src/main/java/com/prumostore/catalog/CatalogApplication.java
@@ -2,8 +2,10 @@ package com.prumostore.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.prumostore")
 public class CatalogApplication {
 
 	public static void main(String[] args) {
